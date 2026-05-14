b198b7f0ec8bdcddc06fc304d6150eb4fcee8bdb
areebaa7 <areebasundal7@gmail.com>
2026-05-14 15:28:28 +0500

    Added update and delete functionality

--- main.cpp
+++ main.cpp
@@ -5,23 +5,50 @@ using namespace std;
 
 int main()
 {
+    int choice;
     string vehicle;
-    char choice;
 
     do
     {
-        cout << "Which vehicle do you want to enter (bike/car): " << endl;
-        getline(cin, vehicle);
-
-        Get_VehicleData(vehicle);
-
-        cout << "Press Y to continue, N to stop: ";
+        cout << "\n===== VEHICLE MANAGEMENT SYSTEM =====\n";
+        cout << "1. Add Vehicle\n";
+        cout << "2. View Vehicles\n";
+        cout << "3. Update Vehicle\n";
+        cout << "4. Delete Vehicle\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
         cin >> choice;
         cin.ignore();
 
-    } while (choice == 'Y' || choice == 'y');
+        switch (choice)
+        {
+            case 1:
+                cout << "Which vehicle do you want to enter (bike/car): ";
+                getline(cin, vehicle);
+                Get_VehicleData(vehicle);
+                break;
 
-    cout << "Program terminated by user." << endl;
+            case 2:
+                Read_VehicleData();
+                break;
+
+            case 3:
+                Update_VehicleData();
+                break;
+
+            case 4:
+                Delete_VehicleData();
+                break;
+
+            case 5:
+                cout << "Program terminated by user." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice." << endl;
+        }
+
+    } while (choice != 5);
 
     return 0;
 }
